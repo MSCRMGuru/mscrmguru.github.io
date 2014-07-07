5dadcf1e2d07e7841fe822f692052cdd6136472b
MSCRMGuru <admin@mscrm.guru>
2014-07-08 00:40:10 +1000

    Site updated at 2014-07-07 14:40:10 UTC

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,3 +4,14 @@ function ContactGuruSubmittedHandler() {
 	$(".thank-you-message").fadeIn();
 
 }
+
+
+
+$(document).ready(function() {
+
+});
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+}); 
